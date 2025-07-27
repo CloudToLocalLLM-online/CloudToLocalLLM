22c8fe5e36afb29dd5fde63d1f6b2a4351adccef
imrightguy <193101532+imrightguy@users.noreply.github.com>
2025-07-27 00:12:27 -0400

    Remove Ansible deployment infrastructure - migrating to VPS-based deployment

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build the Docker image
-docker build -t cloudtolocalllm-build -f config/docker/Dockerfile.build .
-
-# Run the build and package script in the Docker container
-docker run --rm -v "$(pwd)/dist:/home/builder/dist" cloudtolocalllm-build
