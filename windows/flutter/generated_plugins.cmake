176a1c1fd24fe9a1bd42ac96c54dee98d6782b55
imrightguy <193101532+imrightguy@users.noreply.github.com>
2025-07-30 01:10:27 -0400

    Add missing sentry_flutter dependency for v4.0.0 deployment

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   flutter_secure_storage_x_windows
   screen_retriever_windows
+  sentry_flutter
   tray_manager
   url_launcher_windows
   window_manager
