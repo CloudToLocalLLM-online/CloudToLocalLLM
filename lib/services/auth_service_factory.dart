7aeacebd9ad3c7a01b08ae780a96c2f2e043f66a
Christopher Maltais <193101532+imrightguy@users.noreply.github.com>
2025-11-01 17:41:22 -0400

    fix!: correct conditional import in auth factory

--- lib/services/auth_service_factory.dart
+++ lib/services/auth_service_factory.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'auth0_service.dart';
-import 'auth0_web_service.dart' if (dart.library.io) 'auth0_desktop_service.dart';
+import 'auth0_desktop_service.dart' if (dart.library.html) 'auth0_web_service.dart';
 
 Auth0Service createAuth0Service() {
   if (kIsWeb) {
