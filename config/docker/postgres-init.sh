15711778a2dd6514dfca37905658dfd37800ca1d
Christopher Maltais <christopher.maltais@gmail.com>
2025-11-29 03:09:24 -0500

    fix: create database in postgres-init.sh

--- config/docker/postgres-init.sh
+++ config/docker/postgres-init.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "Starting database initialization..."
 
+# Create database if it doesn't exist
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+  SELECT 'CREATE DATABASE $POSTGRES_DB'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_DB')\\gexec
+EOSQL
+
 # Create appuser if it doesn't exist
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   DO \$\$
